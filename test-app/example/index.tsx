a5b9e7a7adebd712743398976dfbf3fb74f1ac9c
Maxwell Mooney <maxwell.mooney@genesys.com>
2021-08-16 12:20:12 -0400

    PCM-1667 adding foundational work for TSDX React Template

---
+++ test-app/example/index.tsx
@@ -0,0 +1,15 @@
+import 'react-app-polyfill/ie11';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Thing } from '../.';
+import HeadsetService from '../../src/services/headset';
+
+const App = () => {
+  return (
+    <div>
+      <Thing />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
