f90458ca6a876bf537b3ab810c06d4edef9610e4
Maxwell Mooney <maxwell.mooney@genesys.com>
2022-04-25 16:44:36 -0400

    no-jira merge conflict fix

--- react-app/src/library/services/vendor-implementations/jabra/jabra.ts
+++ react-app/src/library/services/vendor-implementations/jabra/jabra.ts
@@ -240,13 +240,8 @@ export default class JabraService extends VendorImplementation {
             if (!this.callLock) {
                 return this.logger.info('Currently not in possession of the Call Lock; Cannot react to Device Actions')
             }
-<<<<<<< HEAD
             this.callControl.offHook(false);
             this.callControl.releaseCallLock();
-=======
-            this.callControl.offHook(false),
-            this.callControl.releaseCallLock()
->>>>>>> 29eeb48cf590c3a575ec8ed55b4c209ab8973e66
         } catch ({message, type}) {
             if (this.checkForCallLockError(message, type)) {
                 this.logger.info(message);
