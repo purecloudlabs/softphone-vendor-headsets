c398fd9650cf7b5817d49f1ae8e9f3f0d1707cd9
maxwellmooney13 <83785824+maxwellmooney13@users.noreply.github.com>
2024-01-10 11:23:45 -0500

    no-jira added passed in param to both calls of clearSelectedImplementation

--- react-app/src/library/services/headset.ts
+++ react-app/src/library/services/headset.ts
@@ -105,7 +105,7 @@ export default class HeadsetService {
       if (implementation) {
         this.changeImplementation(implementation, newMicLabel);
       } else if (this.selectedImplementation) {
-        this.clearSelectedImplementation();
+        this.clearSelectedImplementation(changeReason);
       }
     } else {
       this.clearSelectedImplementation(changeReason);
