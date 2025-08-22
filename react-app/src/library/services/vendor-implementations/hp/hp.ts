574c8d7fd3c3455790297d65de85ef6da436525f
Tzakis, Andrew <andrew.tzakis@hp.com>
2025-08-22 11:46:32 -0500

    Initial genesys integration

--- react-app/src/library/services/vendor-implementations/hp/hp.ts
+++ react-app/src/library/services/vendor-implementations/hp/hp.ts
@@ -6,9 +6,9 @@ import CcSdk, { CallState, SdkEvent } from '@hp/call-control-sdk';
 
 const defaultAppName = 'genesys-cloud-headset-library';
 
-export default class HpService extends VendorImplementation {
-  private static instance: HpService;
-  vendorName = 'Hp';
+export default class PlantronicsService extends VendorImplementation {
+  private static instance: PlantronicsService;
+  vendorName = 'Plantronics';
   pluginName: string;
   config: ImplementationConfig;
   pendingDeviceLabel: string | null = null;
@@ -35,15 +35,16 @@ export default class HpService extends VendorImplementation {
   deviceLabelMatchesVendor (label: string): boolean {
     // includes vendor name or vendorId (chrome only)
     const lowerLabel = label.toLowerCase();
-    return ['plantronics', 'plt', 'poly', '(047f:', '(095d:', '(03f0:'].some(searchVal => lowerLabel.includes(searchVal));
+    console.log("deviceLabelMatchesVendor label", label, lowerLabel, ['plantronics', 'plt', 'poly', '(047f:'].some(searchVal => lowerLabel.includes(searchVal)));
+    return ['plantronics', 'plt', 'poly', '(047f:'].some(searchVal => lowerLabel.includes(searchVal));
   }
 
-  static getInstance (config: ImplementationConfig): HpService {
-    if (!HpService.instance || config.createNew) {
-      HpService.instance = new HpService(config);
+  static getInstance (config: ImplementationConfig): PlantronicsService {
+    if (!PlantronicsService.instance || config.createNew) {
+      PlantronicsService.instance = new PlantronicsService(config);
     }
 
-    return HpService.instance;
+    return PlantronicsService.instance;
   }
 
   get deviceName (): string | undefined {
@@ -59,7 +60,7 @@ export default class HpService extends VendorImplementation {
   }
 
   async sdkEventHandler (sdkEvent: SdkEvent): Promise<any> {
-    this.logger.debug('sdkEventHandler', SdkEvent[sdkEvent]);
+    console.log('sdkEventHandler', SdkEvent[sdkEvent]);
 
     switch (sdkEvent) {
     case SdkEvent.CONNECT_SUCCESS:
@@ -168,8 +169,7 @@ export default class HpService extends VendorImplementation {
           return;
         }
         if (activeConversationId) {
-          /* Add this to the end of the stack to simulate a round robin if doing flash with multiple active calls */
-          this.heldConversationIds = [activeConversationId].concat(this.heldConversationIds);
+          this.heldConversationIds.push(activeConversationId);
           this.deviceHoldStatusChanged({
             holdRequested: true,
             name: SdkEvent[sdkEvent],
@@ -185,18 +185,17 @@ export default class HpService extends VendorImplementation {
             conversationId: this.incomingConversationId,
           });
           this.incomingConversationId = null;
-        } else if (this.activeConversationIds.length == 0 && (this.heldConversationIds.length > 1 || !activeConversationId)) {
-          /* Ensure there are no active calls before resuming a held call, and either resuming a previous held call.
-           * if activeConverstationId is not null then we just held the call above so we only resume if there was more
-           * than one held call when activeConversationId is non null.*/
+        } else {
           const heldConversationId = this.heldConversationIds.pop();
-          this.activeConversationIds.push(heldConversationId);
-          this.deviceHoldStatusChanged({
-            holdRequested: false,
-            name: SdkEvent[sdkEvent],
-            code: sdkEvent,
-            conversationId: heldConversationId,
-          });
+          if (heldConversationId) {
+            this.activeConversationIds.push(heldConversationId);
+            this.deviceHoldStatusChanged({
+              holdRequested: false,
+              name: SdkEvent[sdkEvent],
+              code: sdkEvent,
+              conversationId: heldConversationId,
+            });
+          }
         }
       }
       break;
@@ -204,12 +203,10 @@ export default class HpService extends VendorImplementation {
     case SdkEvent.MUTE:
     case SdkEvent.UNMUTE:
       {
-        let activeConversationId: string;
         if (this.activeConversationIds.length < 1) {
           this.logger.warn('No active call to mute or unmute.');
-        } else {
-          activeConversationId = this.activeConversationIds[0];
         }
+        const activeConversationId = this.activeConversationIds[0];
         this.deviceMuteChanged({
           isMuted: sdkEvent == SdkEvent.MUTE,
           name: SdkEvent[sdkEvent],
@@ -231,53 +228,52 @@ export default class HpService extends VendorImplementation {
 
   async connect (originalDeviceLabel: string): Promise<any> {
     !this.isConnecting && this.changeConnectionStatus({ isConnected: this.isConnected, isConnecting: true });
-
-    try {
-      if (!this.ccsdkRegistered) {
-        this.ccsdkRegistered = await this.callControlSdk.registerEventHandler(this.sdkEventHandler.bind(this));
-        this.logger.debug('CCSDK Registered', this.ccsdkRegistered);
-      }
-
-      await this.callControlSdk.disconnectHeadset();
-
-      const deviceLabel = originalDeviceLabel.toLocaleLowerCase();
-      this._device = await this.getPreviouslyConnectedDevice(deviceLabel);
-
-      if (this._device != null) {
-        let validConnect = false;
-        validConnect = await this.callControlSdk.connectHeadset(this._device);
-        this.logger.debug('connect Headset validConnect', validConnect);
-        if (!validConnect) {
-          this.pendingDeviceLabel = null;
-          this.isConnecting && this.changeConnectionStatus({ isConnected: false, isConnecting: false });
-          return;
-        } else {
-          this.pendingDeviceLabel = deviceLabel;
-        }
-      }
-      else {
-        this.logger.debug('No previously connected device found for ', deviceLabel);
-        try {
-          this._device = await this.getDeviceFromWebhid();
-        } catch (e) {
-          this.isConnecting &&
-            this.changeConnectionStatus({ isConnected: this.isConnected, isConnecting: false });
-          return;
-        }
-      }
-
-      this.logger.info('Device found', this._device);
+    console.log('Connecting to Plantronics/Poly headset...');
+    if (!this.ccsdkRegistered) {
+      this.ccsdkRegistered = await this.callControlSdk.registerEventHandler(this.sdkEventHandler.bind(this));
+      console.log('CCSDK Registered', this.ccsdkRegistered);
     }
-    finally
-    {
-      this.isConnecting && this.changeConnectionStatus({ isConnected: this.isConnected, isConnecting: true });
+
+    await this.callControlSdk.disconnectHeadset();
+
+    const deviceLabel = originalDeviceLabel.toLocaleLowerCase();
+    this._device = await this.getPreviouslyConnectedDevice(deviceLabel);
+
+    if (this._device != null) {
+      let validConnect = false;
+      validConnect = await this.callControlSdk.connectHeadset(this._device);
+      console.log('connect Headset validConnect', validConnect);
+      if (!validConnect) {
+        this.pendingDeviceLabel = null;
+        this.isConnecting && this.changeConnectionStatus({ isConnected: false, isConnecting: false });
+      } else {
+        this.pendingDeviceLabel = deviceLabel;
+      }
     }
+    else {
+      console.log('No previously connected device found for ', deviceLabel);
+      try {
+        this._device = await this.getDeviceFromWebhid(deviceLabel);
+      } catch (e) {
+        this.isConnecting &&
+          this.changeConnectionStatus({ isConnected: this.isConnected, isConnecting: false });
+        return;
+      }
+    }
+
+    console.log('Device found', this._device);
+
+
+    // .finally(() => {
+    //   this.isConnecting && this.changeConnectionStatus({ isConnected: this.isConnected, isConnecting: false });
+    //});
   }
 
   async getPreviouslyConnectedDevice (deviceLabel: string): Promise<any> {
     const allowedHIDDevices = await (window.navigator as any).hid.getDevices();
     for (const device of allowedHIDDevices) {
       if (deviceLabel.includes(device?.productName?.toLowerCase())) {
+        console.log('Found previously connected device');
         return device;
       }
     }
@@ -285,16 +281,18 @@ export default class HpService extends VendorImplementation {
   }
 
   async webHidPairing (): Promise<any> {
+    console.log('webHidPairing');
+
     // Done this way in order to validate the device label
     // If this is the way we go, then perhaps the filters should be defined in ccsdk
     const deviceFilters = [{ "vendorId": 0x047f }, { "vendorId": 0x095d }, { "vendorId": 0x03f0 }];
     const devices = await (window.navigator as any).hid.requestDevice({ filters: deviceFilters });
     const headset = devices[0];
     if (!headset) {
-      this.logger.warn('webHidPairing: No headset found');
+      console.error('No headset found');
       this.isConnecting && this.changeConnectionStatus({ isConnected: false, isConnecting: false });
     } else if (this.pendingDeviceLabel && !this.pendingDeviceLabel.includes(headset.productName.toLowerCase())) {
-      this.logger.error('webHidPairing: Device label does not match', this.pendingDeviceLabel, headset.productName);
+      console.error('Device label does not match', this.pendingDeviceLabel, headset.productName);
       this.pendingDeviceLabel = null;
       this.isConnecting && this.changeConnectionStatus({ isConnected: false, isConnecting: false });
       const err = new Error('The selected device was not granted WebHID permissions');
@@ -306,15 +304,21 @@ export default class HpService extends VendorImplementation {
       this._device = headset;
       if (!validConnect) {
         this.isConnecting && this.changeConnectionStatus({ isConnected: false, isConnecting: false });
-      } else {
-        !this.isConnecting && this.changeConnectionStatus({ isConnected: false, isConnecting: true });
       }
     }
 
     this.pendingDeviceLabel = null;
+
+    // This would be a way without validating the device label
+    //let validConnect = false;
+    //validConnect = await this.callControlSdk.connectHeadset();
+    //if (!validConnect) {
+    //  this.isConnecting && this.changeConnectionStatus({ isConnected: false, isConnecting: false });
+    //}
   }
 
-  async getDeviceFromWebhid (): Promise<any> {
+  async getDeviceFromWebhid (deviceLabel: string): Promise<any> {
+    console.log('getDeviceFromWebhid', deviceLabel);
     this.requestWebHidPermissions(this.webHidPairing.bind(this));
   }
 
@@ -322,11 +326,9 @@ export default class HpService extends VendorImplementation {
     if (!this.isConnected) {
       return;
     }
-
     if (clearReason !== 'alternativeClient') {
-      await this.callControlSdk.disconnectHeadset();
+      //  await this.unregisterPlugin();
     }
-
     this._deviceInfo = null;
     this.isConnected && this.changeConnectionStatus({ isConnected: false, isConnecting: this.isConnecting });
   }
@@ -336,8 +338,13 @@ export default class HpService extends VendorImplementation {
     let remainingActiveCalls = false;
     let remainingHeldCalls = false;
 
-    remainingActiveCalls = this.activeConversationIds.length > 0;
-    remainingHeldCalls = this.heldConversationIds.length > 0;
+    if (this.activeConversationIds) {
+      remainingActiveCalls = this.activeConversationIds.length > 0;
+    }
+
+    if (this.heldConversationIds) {
+      remainingHeldCalls = this.heldConversationIds.length > 0;
+    }
 
     if (this.incomingConversationId != null) {
       callState = CallState.INCOMING;
@@ -354,14 +361,14 @@ export default class HpService extends VendorImplementation {
     }
 
     this.callControlSdk.setCallState(callState);
-    this.logger.info('CCSDK Call State Updated to:', CallState[callState]);
+    console.log('#### CCSDK Call State Updated to:', CallState[callState]);
   }
 
   removeConversationId (conversationId: string): void {
-    if (this.activeConversationIds.length > 0) {
+    if (this.activeConversationIds) {
       this.activeConversationIds = this.activeConversationIds.filter(id => id !== conversationId);
     }
-    if (this.heldConversationIds.length > 0) {
+    if (this.heldConversationIds) {
       this.heldConversationIds = this.heldConversationIds.filter(id => id !== conversationId);
     }
   }
@@ -369,34 +376,35 @@ export default class HpService extends VendorImplementation {
   async incomingCall (callInfo: CallInfo): Promise<void> {
     this.logger.info('Inside incomingCall of selected implementation (Plantronics/Poly)');
 
-    if (!callInfo || !callInfo.conversationId) {
+    if (!callInfo.conversationId) {
       throw new Error('Must provide conversationId');
     }
 
     if (this.incomingConversationId != null) {
       const message = `Incoming call for conversationId ${callInfo.conversationId} while another call is pending with conversationId ${this.incomingConversationId}`;
-      this.logger.warn(message);
+      console.warn(message);
+      this.logger.info(message);
     }
 
     this.incomingConversationId = callInfo.conversationId;
     this.updateCcsdkCallState();
   }
 
-  async outgoingCall (callInfo: CallInfo): Promise<any> {
-    if (!callInfo || !callInfo.conversationId) {
+  async outgoingCall ({ conversationId, contactName }: CallInfo): Promise<any> {
+    console.log('Outgoing call for conversationId:', conversationId, ' contactName', contactName);
+    if (!conversationId) {
       throw new Error('Must provide conversationId');
     }
-    this.logger.info('Outgoing call for conversationId:', callInfo.conversationId, ' contactName', callInfo.contactName);
 
-    if (!this.activeConversationIds.includes(callInfo.conversationId)) {
-      this.activeConversationIds.push(callInfo.conversationId);
+    if (!this.activeConversationIds.includes(conversationId)) {
+      this.activeConversationIds.push(conversationId);
     }
 
     this.updateCcsdkCallState();
   }
 
   async answerCall (conversationId: string, autoAnswer?: boolean): Promise<any> {
-    this.logger.info('Answering call for conversationId:', conversationId, ' auto', autoAnswer);
+    console.log('Answering call for conversationId:', conversationId, ' auto', autoAnswer);
     if (autoAnswer) {
       await this.incomingCall({ conversationId });
     }
@@ -410,20 +418,21 @@ export default class HpService extends VendorImplementation {
   }
 
   async rejectCall (conversationId: string): Promise<any> {
-    this.logger.info('Rejecting call for conversationId:', conversationId);
+    console.log('Rejecting call for conversationId:', conversationId);
     this.incomingConversationId = null;
     this.removeConversationId(conversationId);
     this.updateCcsdkCallState();
   }
 
   async endCall (conversationId: string): Promise<any> {
-    this.logger.info('End call for conversationId:', conversationId);
+    console.log('End call for conversationId:', conversationId);
     if (!conversationId) {
-      this.logger.error('conversationId is invalid');
+      throw new Error('conversationId is invalid');
     }
 
     if (!this.activeConversationIds || this.activeConversationIds.length === 0) {
       const message = `End call requested for conversationId ${conversationId} but no active call is present`;
+      console.warn(message);
       this.logger.info(message);
     }
 
@@ -432,7 +441,7 @@ export default class HpService extends VendorImplementation {
   }
 
   async endAllCalls (): Promise<void> {
-    this.logger.info('End all calls');
+    console.log('End all calls');
     this.activeConversationIds = [];
     this.heldConversationIds = [];
     this.incomingConversationId = null;
@@ -440,18 +449,22 @@ export default class HpService extends VendorImplementation {
   }
 
   async setMute (value: boolean): Promise<any> {
-    this.logger.info('setMute to:', value);
+    console.log('setMute to:', value);
     this.callControlSdk.setMuteState(value);
   }
 
   async setHold (conversationId: string, value: boolean): Promise<any> {
-    this.logger.info('setHold', conversationId, value);
+    console.log('setHold', conversationId, value);
 
     this.removeConversationId(conversationId);
     if (value) {
-      this.heldConversationIds.push(conversationId);
+      if (!this.heldConversationIds.includes(conversationId)) {
+        this.heldConversationIds.push(conversationId);
+      }
     } else {
-      this.activeConversationIds.push(conversationId);
+      if (!this.activeConversationIds.includes(conversationId)) {
+        this.activeConversationIds.push(conversationId);
+      }
     }
     this.updateCcsdkCallState();
   }
