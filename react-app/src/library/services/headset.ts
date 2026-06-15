c33b37fa070cd3dead8e51b8afa59a98d707ed9c
Maxwell Mooney <maxwellmooney13@gmail.com>
2026-06-15 13:36:00 -0400

    [STREAM-1660] - Reintroduce Plantronics implementation as a fallback if feature toggle is disabled

--- react-app/src/library/services/headset.ts
+++ react-app/src/library/services/headset.ts
@@ -1,6 +1,7 @@
 import { Observable, Subject } from 'rxjs';
 import { VendorImplementation, ImplementationConfig } from './vendor-implementations/vendor-implementation';
 import CyberAcousticsService from './vendor-implementations/CyberAcoustics/CyberAcoustics';
+import PlantronicsService from './vendor-implementations/plantronics/plantronics';
 import HpService from './vendor-implementations/hp/hp';
 import SennheiserService from './vendor-implementations/sennheiser/sennheiser';
 import JabraService from './vendor-implementations/jabra/jabra';
@@ -21,6 +22,7 @@ const REMOVE_WAIT = 2000;
 export default class HeadsetService {
   private static instance: HeadsetService;
 
+  plantronics: VendorImplementation;
   hp: VendorImplementation;
   jabraNative: VendorImplementation;
   jabra: VendorImplementation;
@@ -40,7 +42,8 @@ export default class HeadsetService {
     this.headsetEvents$ = this._headsetEvents$.asObservable();
 
     this.logger = config.logger || console;
-    this.hp = HpService.getInstance({ logger: this.logger, appName: config.appName });
+    this.plantronics = PlantronicsService.getInstance({ logger: this.logger, appName: config.appName, useNewPolyImplementation: config.useNewPolyImplementation });
+    this.hp = HpService.getInstance({ logger: this.logger, appName: config.appName, useNewPolyImplementation: config.useNewPolyImplementation });
     this.jabraNative = JabraNativeService.getInstance({ logger: this.logger });
     this.jabra = JabraService.getInstance({ logger: this.logger });
     this.sennheiser = SennheiserService.getInstance({ logger: this.logger });
@@ -48,7 +51,7 @@ export default class HeadsetService {
     this.vbet = VBetService.getInstance({ logger: this.logger, hostedContext: config.hostedContext, useWebHidOnDesktopVbet: config.useWebHidOnDesktopVbet });
     this.cyberAcoustics = CyberAcousticsService.getInstance({ logger: this.logger, hostedContext: config.hostedContext, useWebHidOnDesktopCyberAcoustics: config.useWebHidOnDesktopCyberAcoustics });
 
-    [this.hp, this.jabra, this.jabraNative, this.sennheiser, this.yealink, this.vbet, this.cyberAcoustics].forEach(implementation => this.subscribeToHeadsetEvents(implementation));
+    [this.plantronics, this.hp, this.jabra, this.jabraNative, this.sennheiser, this.yealink, this.vbet, this.cyberAcoustics].forEach(implementation => this.subscribeToHeadsetEvents(implementation));
   }
 
   static getInstance (config: ImplementationConfig): HeadsetService {
@@ -61,7 +64,9 @@ export default class HeadsetService {
 
   get implementations (): VendorImplementation[] {
     const implementations = [
+
       this.sennheiser,
+      this.plantronics,
       this.hp,
       this.jabra,
       this.jabraNative,
